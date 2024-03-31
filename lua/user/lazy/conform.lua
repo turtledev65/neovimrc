d1641f1862e07b29756d67efbcc1154a1459b590
turtledev <turtledev65@gmail.com>
2024-03-31 15:15:21 +0300

    make slow formatters format asynchronously

--- lua/user/lazy/conform.lua
+++ lua/user/lazy/conform.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = {
-      timeout_ms = 200,
+      timeout_ms = 500,
       lsp_fallback = true,
     },
     formatters_by_ft = {
@@ -13,13 +13,43 @@ return {
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
-      javascript = { { 'prettierd', 'prettier' } },
-      html = { { 'prettierd', 'prettier' } },
+      javascript = { 'prettier' },
+      typescript = { 'prettier' },
+      javascriptreact = { 'prettier' },
+      typescriptreact = { 'prettier' },
+      html = { 'prettier' },
     },
     formatters = {
       stylua = {
         prepend_args = { '--indent-type', 'Spaces', '--indent-width', 2, '--quote-style', 'AutoPreferSingle' },
       },
+      prettier = {
+        prepend_args = { '--config-precedence', 'prefer-file', '--arrow-parens', 'avoid', '--tab-width', 2 },
+      },
     },
   },
+  config = function()
+    local slow_format_filetypes = {}
+    require('conform').setup({
+      format_on_save = function(bufnr)
+        if slow_format_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+        local function on_format(err)
+          if err and err:match('timeout$') then
+            slow_format_filetypes[vim.bo[bufnr].filetype] = true
+          end
+        end
+
+        return { timeout_ms = 200, lsp_fallback = true }, on_format
+      end,
+
+      format_after_save = function(bufnr)
+        if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+        return { lsp_fallback = true }
+      end,
+    })
+  end,
 }
